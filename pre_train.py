8207b20931ea935a2dcab84a7c703d96b91175c7
Barristen <2246398498@qq.com>
2024-03-11 12:32:31 +0000

    update

--- pre_train.py
+++ pre_train.py
@@ -5,10 +5,11 @@ from ProG.utils import mkdir
 	
 seed_everything(1)
 mkdir('./pre_trained_gnn/')
-
+pt = SimGRACE(dataset_name = 'Cora', gnn_type = 'GCN', hid_dim = 128, gln =3, num_epoch=50)
+# pt = GraphCL(dataset_name = 'Cora', gnn_type = 'GCN', hid_dim = 128, gln =3, num_epoch=50)
 # pt = Edgepred_GPPT(dataset_name = 'Cora', gnn_type = 'GCN', hid_dim = 128, gln =3, num_epoch=100)
 
-pt = Edgepred_GPPT(dataset_name = 'Cora', gnn_type = 'GCN', hid_dim = 128, gln =3, num_epoch=100)
+# pt = Edgepred_GPPT(dataset_name = 'Cora', gnn_type = 'GCN', hid_dim = 128, gln =3, num_epoch=100)
 # pt = Edgepred_Gprompt(dataset_name = 'Cora', gnn_type = 'GCN', hid_dim = 128, gln =3, num_epoch=100)
 # pt = GraphCL(dataset_name = 'ENZYMES', gnn_type = 'GCN', hid_dim = 128, gln =3, num_epoch=50)
 # pt = SimGRACE(dataset_name = 'ENZYMES', gnn_type = 'GCN', hid_dim = 128, gln =3, num_epoch=50)
