5075f3631039101629622669e521080e0433bb77
Barristen <2246398498@qq.com>
2024-03-14 06:33:25 +0000

    update

--- pre_train.py
+++ pre_train.py
@@ -3,7 +3,7 @@ from ProG.utils import seed_everything
 from ProG.utils import mkdir, get_args
 
 
-args = get_args
+args = get_args()
 seed_everything(args.seed)
 mkdir('./pre_trained_gnn/')
 
@@ -11,9 +11,9 @@ if args.task == 'SimGRACE':
     pt = SimGRACE(dataset_name = args.dataset_name, gnn_type = args.gnn_type, hid_dim = args.hid_dim, gln = args.num_layer, num_epoch=args.epochs)
 if args.task == 'GraphCL':
     pt = GraphCL(dataset_name = args.dataset_name, gnn_type = args.gnn_type, hid_dim = args.hid_dim, gln = args.num_layer, num_epoch=args.epochs)
-if args.task == 'GraphCL':
+if args.task == 'Edgepred_GPPT':
     pt = Edgepred_GPPT(dataset_name = args.dataset_name, gnn_type = args.gnn_type, hid_dim = args.hid_dim, gln = args.num_layer, num_epoch=args.epochs)
-if args.task == 'GraphCL':
+if args.task == 'Edgepred_Gprompt':
     pt = Edgepred_Gprompt(dataset_name = args.dataset_name, gnn_type = args.gnn_type, hid_dim = args.hid_dim, gln = args.num_layer, num_epoch=args.epochs)
 
 pt.pretrain()
