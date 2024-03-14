1e30f7f0b995c96c357c7b408627a9a38de29c97
Barristen <2246398498@qq.com>
2024-03-14 06:39:29 +0000

    update

--- downstream_task.py
+++ downstream_task.py
@@ -1,85 +1,23 @@
-from ProG.tasker import NodeTask, LinkTask, GraphTask
-from ProG.prompt import GPF, GPF_plus, GPPTPrompt, GPrompt, LightPrompt
+from ProG.tasker import NodeTask, GraphTask
 from ProG.utils import seed_everything
 from torchsummary import summary
 from ProG.utils import print_model_parameters
-
+from ProG.utils import  get_args
 # gnn_type =  'GCN' 'GAT' 'GT' 'GCov' 'GIN' 'GraphSAGE'
 # prompt_type = 'All-in-one', 'GPF', GPF-plus', 'GPPT', 'Gprompt'
 	
-seed_everything(1)
+
+args = get_args()
+seed_everything(args.seed)
 
 ### NodeTask
-
-# tasker = NodeTask(pre_train_model_path = 'None', 
-#                   dataset_name = 'Cora', num_layer = 3, gnn_type = 'GCN', prompt_type = 'None', epochs = 150, shot_num = 5)
-
-# tasker = NodeTask(pre_train_model_path = './pre_trained_gnn/Cora.Edgepred_Gprompt.GCN.128hidden_dim.pth', 
-#                   dataset_name = 'Cora', num_layer = 3, gnn_type = 'GCN', prompt_type = 'GPPT', epochs = 150, shot_num = 5)
-
-tasker = NodeTask(pre_train_model_path = './pre_trained_gnn/Cora.GraphCL.GCN.128hidden_dim.pth', 
-                  dataset_name = 'Cora', num_layer = 3, gnn_type = 'GCN', prompt_type = 'GPF-plus', epochs = 150, shot_num = 5)
-
-# tasker = NodeTask(pre_train_model_path = './pre_trained_gnn/Cora.Edgepred_Gprompt.GCN.128hidden_dim.pth', 
-#                   dataset_name = 'Cora', num_layer = 3, gnn_type = 'GCN', prompt_type = 'GPF-plus', epochs = 150, shot_num = 5) bug
-
-# tasker = NodeTask(pre_train_model_path = 'None', 
-#                   dataset_name = 'Cora', num_layer = 3, gnn_type = 'GCN', prompt_type = 'None', shot_num = 5)
-
-# tasker = NodeTask(pre_train_model_path = 'None', 
-#                   dataset_name = 'Cora', num_layer = 3, gnn_type = 'GCN', prompt_type = 'None', shot_num = 5)
-
-# tasker = NodeTask(pre_train_model_path = 'None', 
-#                   dataset_name = 'Cora', num_layer = 3, gnn_type = 'GAT', prompt_type = 'None', shot_num = 10)
-
-# tasker = NodeTask(pre_train_model_path = './pre_trained_gnn/Cora.Edgepred_GPPT.GCN.128hidden_dim.pth', 
-#                   dataset_name = 'Cora', num_layer = 3, gnn_type = 'GCN', prompt_type = 'None', shot_num = 10)
-
-# tasker = NodeTask(pre_train_model_path = './pre_trained_gnn/Cora.Edgepred_GPPT.GCN.128hidden_dim.pth', 
-#                   dataset_name = 'Cora', num_layer = 3, gnn_type = 'GCN', prompt_type = 'GPPT', shot_num = 1)
+if args.task == 'NodeTask':
+    tasker = NodeTask(pre_train_model_path = 'None', 
+                    dataset_name = 'Cora', num_layer = 3, gnn_type = 'GCN', prompt_type = 'None', epochs = 150, shot_num = 5)
+    tasker.run()
 
 
-### GraphTask
-
-
-
-# MUTAG dataset
-# tasker = GraphTask(pre_train_model_path = './pre_trained_gnn/MUTAG.SimGRACE.GCN.128hidden_dim.pth', 
-#                      dataset_name = 'MUTAG', num_layer = 3, gnn_type = 'GCN', prompt_type = 'All-in-one', shot_num = 10)
-
-# tasker = GraphTask(pre_train_model_path = 'None', 
-#                      dataset_name = 'MUTAG', num_layer = 3, gnn_type = 'GCN', prompt_type = 'None', shot_num = 10)
-
-# tasker = GraphTask(pre_train_model_path = './pre_trained_gnn/MUTAG.SimGRACE.GCN.128hidden_dim.pth', 
-#                      dataset_name = 'MUTAG', num_layer = 3, gnn_type = 'GCN', prompt_type = 'None', shot_num = 10)
-
-# tasker = GraphTask(pre_train_model_path = './pre_trained_gnn/MUTAG.SimGRACE.GCN.128hidden_dim.pth', 
-#                      dataset_name = 'MUTAG', num_layer = 3, gnn_type = 'GCN', prompt_type = 'GPF', shot_num = 10)
-
-# tasker = GraphTask(pre_train_model_path = './pre_trained_gnn/MUTAG.SimGRACE.GCN.128hidden_dim.pth', 
-#                      dataset_name = 'MUTAG', num_layer = 3, gnn_type = 'GCN', prompt_type = 'GPF-plus', shot_num = 10)
-
-# tasker = GraphTask(pre_train_model_path = './pre_trained_gnn/MUTAG.SimGRACE.GCN.128hidden_dim.pth', 
-#                      dataset_name = 'MUTAG', num_layer = 3, gnn_type = 'GCN', prompt_type = 'Gprompt', shot_num = 10)
-
-# ENZYMES dataset
-# tasker = GraphTask(pre_train_model_path = './pre_trained_gnn/ENZYMES.GraphCL.GCN.128hidden_dim.pth', 
-#                      dataset_name = 'ENZYMES', num_layer = 3, gnn_type = 'GCN', prompt_type = 'All-in-one', shot_num = 10)
-
-# tasker = GraphTask(pre_train_model_path = 'None', 
-#                      dataset_name = 'ENZYMES', num_layer = 3, gnn_type = 'GraphSAGE', prompt_type = 'None', shot_num = 10)
-
-# tasker = GraphTask(pre_train_model_path = './pre_trained_gnn/ENZYMES.GraphCL.GCN.128hidden_dim.pth', 
-#                      dataset_name = 'ENZYMES', num_layer = 3, gnn_type = 'GCN', prompt_type = 'None', shot_num = 10)
-
-# tasker = GraphTask(pre_train_model_path = './pre_trained_gnn/MUTAG.SimGRACE.GCN.128hidden_dim.pth', 
-#                      dataset_name = 'ENZYMES', num_layer = 3, gnn_type = 'GCN', prompt_type = 'GPF', shot_num = 10)
-
-# tasker = GraphTask(pre_train_model_path = './pre_trained_gnn/MUTAG.SimGRACE.GCN.128hidden_dim.pth', 
-#                      dataset_name = 'ENZYMES', num_layer = 3, gnn_type = 'GCN', prompt_type = 'GPF-plus', shot_num = 10)
-
-# tasker = GraphTask(pre_train_model_path = './pre_trained_gnn/MUTAG.SimGRACE.GCN.128hidden_dim.pth', 
-#                      dataset_name = 'ENZYMES', num_layer = 3, gnn_type = 'GCN', prompt_type = 'Gprompt', shot_num = 10)
-
-
-tasker.run()
+if args.task == 'GraphTask':
+    tasker = GraphTask(pre_train_model_path = 'None', 
+                    dataset_name = 'MUTAG', num_layer = 3, gnn_type = 'GCN', prompt_type = 'None', epochs = 150, shot_num = 5)
+    tasker.run()
