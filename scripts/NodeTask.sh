b84d7667eb8e94b8934d7d7f79ee44b6a7f90525
Barristen <2246398498@qq.com>
2024-03-15 10:13:19 +0000

    update

---
+++ scripts/NodeTask.sh
@@ -0,0 +1,18 @@
+cd ..
+# Node Task
+    
+# GCN
+python downstream_task.py --pre_train_path 'None' --task NodeTask --dataset_name 'Cora' --gnn_type 'GCN' --prompt_type 'None' --shot_num 10 --hid_dim 128 --num_layer 3 --epochs 50 --seed 42 --device 5
+
+# Edgepred + GCN pretrain-fine_tune
+python downstream_task.py --pre_train_path './pre_trained_gnn/Cora.Edgepred_GPPT.GCN.128hidden_dim.pth' --task NodeTask --dataset_name 'Cora' --gnn_type 'GCN' --prompt_type 'None' --shot_num 10 --hid_dim 128 --num_layer 3 --epochs 50 --seed 42 --device 5
+
+# Edgepred + GPPT + GCN
+python downstream_task.py --pre_train_path './pre_trained_gnn/Cora.Edgepred_GPPT.GCN.128hidden_dim.pth' --task NodeTask --dataset_name 'Cora' --gnn_type 'GCN' --prompt_type 'GPPT' --shot_num 10 --hid_dim 128 --num_layer 3 --epochs 50 --seed 42 --device 5
+
+# Edgepred + All-in-one + GCN
+python downstream_task.py --pre_train_path './pre_trained_gnn/Cora.Edgepred_GPPT.GCN.128hidden_dim.pth' --task NodeTask --dataset_name 'Cora' --gnn_type 'GCN' --prompt_type 'All-in-one' --shot_num 10 --hid_dim 128 --num_layer 3 --epochs 50 --seed 42 --device 5
+
+# Edgepred + Gprompt + GCN
+python downstream_task.py --pre_train_path './pre_trained_gnn/Cora.Edgepred_GPprompt.GCN.128hidden_dim.pth' --task NodeTask --dataset_name 'Cora' --gnn_type 'GCN' --prompt_type 'Gprompt' --shot_num 10 --hid_dim 128 --num_layer 3 --epochs 50 --seed 42 --device 5
+
