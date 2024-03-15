d72a4f5b309cfbb0929cb462214a614eea07199a
Barristen <2246398498@qq.com>
2024-03-15 11:23:56 +0000

    update

--- downstream_task.py
+++ downstream_task.py
@@ -12,6 +12,7 @@ seed_everything(args.seed)
 if args.task == 'NodeTask':
     tasker = NodeTask(pre_train_model_path = args.pre_train_model_path, 
                     dataset_name = args.dataset_name, num_layer = args.num_layer, gnn_type = args.gnn_type, prompt_type = args.prompt_type, epochs = args.epochs, shot_num = args.shot_num)
+    
     tasker.run()
 
 
