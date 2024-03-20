e9c04a27501120f085799542af4ddfd7eda277f3
Barristen <2246398498@qq.com>
2024-03-20 07:21:20 +0000

    update

--- scripts/pre_train.sh
+++ scripts/pre_train.sh
@@ -1,8 +1,8 @@
 
 cd ..
 
-python pre_rain.py --task Edgepred_Gprompt --dataset_name 'Cora' --gnn_type 'GCN' --hid_dim 128 --num_layer 3 --epochs 50 --seed 42 --device 5
-python pre_rain.py --task Edgepred_GPPT --dataset_name 'Cora' --gnn_type 'GCN' --hid_dim 128 --num_layer 3 --epochs 50 --seed 42 --device 5
-python pre_rain.py --task SimGRACE --dataset_name 'Cora' --gnn_type 'GCN' --hid_dim 128 --num_layer 3 --epochs 50 --seed 42 --device 5
-python pre_rain.py --task GraphCL --dataset_name 'Cora' --gnn_type 'GCN' --hid_dim 128 --num_layer 3 --epochs 50 --seed 42 --device 5
+python pre_train.py --task Edgepred_Gprompt --dataset_name 'PROTEIN' --gnn_type 'GCN' --hid_dim 128 --num_layer 3 --epochs 50 --seed 42 --device 5
+python pre_train.py --task Edgepred_GPPT --dataset_name 'Cora' --gnn_type 'GCN' --hid_dim 128 --num_layer 3 --epochs 50 --seed 42 --device 5
+python pre_train.py --task SimGRACE --dataset_name 'PROTEINS' --gnn_type 'GCN' --hid_dim 128 --num_layer 3 --epochs 50 --seed 42 --device 5
+python pre_train.py --task GraphCL --dataset_name 'PROTEINS' --gnn_type 'GCN' --hid_dim 128 --num_layer 3 --epochs 50 --seed 42 --device 5
 
