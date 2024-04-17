2e1c5a16635bed74a9c234b700c0c0f8615b14ca
Barristen <65346840+Barristen@users.noreply.github.com>
2024-04-17 16:52:43 +0800

    Update __init__.py

--- prompt_graph/pretrain/__init__.py
+++ prompt_graph/pretrain/__init__.py
@@ -3,3 +3,4 @@ from.Edgepred_Gprompt import Edgepred_Gprompt
 from.GraphCL import GraphCL
 from.SimGRACE import SimGRACE
 from.MultiGPrompt import PrePrompt, prompt_pretrain_sample
+from.DGI import DGI
