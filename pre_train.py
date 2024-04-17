9c017c99f61d15f6fa8bc995b08c7ed6198a6abd
Barristen <65346840+Barristen@users.noreply.github.com>
2024-04-17 16:53:12 +0800

    Update pre_train.py

--- pre_train.py
+++ pre_train.py
@@ -1,4 +1,4 @@
-from prompt_graph.pretrain import Edgepred_GPPT, Edgepred_Gprompt, GraphCL, SimGRACE, PrePrompt
+from prompt_graph.pretrain import Edgepred_GPPT, Edgepred_Gprompt, GraphCL, SimGRACE, PrePrompt, DGI
 from prompt_graph.utils import seed_everything
 from prompt_graph.utils import mkdir, get_args
 
@@ -15,6 +15,8 @@ if args.task == 'Edgepred_GPPT':
     pt = Edgepred_GPPT(dataset_name = args.dataset_name, gnn_type = args.gnn_type, hid_dim = args.hid_dim, gln = args.num_layer, num_epoch=args.epochs, device=args.device)
 if args.task == 'Edgepred_Gprompt':
     pt = Edgepred_Gprompt(dataset_name = args.dataset_name, gnn_type = args.gnn_type, hid_dim = args.hid_dim, gln = args.num_layer, num_epoch=args.epochs, device=args.device)
+if args.task == 'DGI':
+    pt = DGI(dataset_name = args.dataset_name, gnn_type = args.gnn_type, hid_dim = args.hid_dim, gln = args.num_layer, num_epoch=args.epochs, device=args.device)
 if args.task == 'MultiGprompt':
     nonlinearity = 'prelu'
     pt = PrePrompt(args.dataset_name, args.hid_dim, nonlinearity, 0.9, 0.9, 0.1, 0.001, 2, 0.3)
