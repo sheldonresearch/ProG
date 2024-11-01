6c04135dd268dec94cc97f19f0ddbd9f17c6cda2
WuZhen <498721344@qq.com>
2024-11-01 14:32:04 +0800

    修复pre_train.py中，pretrain_task参数名不匹配的bug
    Signed-off-by: WuZhen <498721344@qq.com>

--- pre_train.py
+++ pre_train.py
@@ -7,23 +7,23 @@ args = get_args()
 seed_everything(args.seed)
 
 
-if args.task == 'SimGRACE':
+if args.pretrain_task == 'SimGRACE':
     pt = SimGRACE(dataset_name = args.dataset_name, gnn_type = args.gnn_type, hid_dim = args.hid_dim, gln = args.num_layer, num_epoch=args.epochs, device=args.device)
-if args.task == 'GraphCL':
+if args.pretrain_task == 'GraphCL':
     pt = GraphCL(dataset_name = args.dataset_name, gnn_type = args.gnn_type, hid_dim = args.hid_dim, gln = args.num_layer, num_epoch=args.epochs, device=args.device)
-if args.task == 'Edgepred_GPPT':
+if args.pretrain_task == 'Edgepred_GPPT':
     pt = Edgepred_GPPT(dataset_name = args.dataset_name, gnn_type = args.gnn_type, hid_dim = args.hid_dim, gln = args.num_layer, num_epoch=args.epochs, device=args.device)
-if args.task == 'Edgepred_Gprompt':
+if args.pretrain_task == 'Edgepred_Gprompt':
     pt = Edgepred_Gprompt(dataset_name = args.dataset_name, gnn_type = args.gnn_type, hid_dim = args.hid_dim, gln = args.num_layer, num_epoch=args.epochs, device=args.device)
-if args.task == 'DGI':
+if args.pretrain_task == 'DGI':
     pt = DGI(dataset_name = args.dataset_name, gnn_type = args.gnn_type, hid_dim = args.hid_dim, gln = args.num_layer, num_epoch=args.epochs, device=args.device)
-if args.task == 'NodeMultiGprompt':
+if args.pretrain_task == 'NodeMultiGprompt':
     nonlinearity = 'prelu'
-    pt = NodePrePrompt(args.dataset_name, args.hid_dim, nonlinearity, 0.9, 0.9, 0.1, 0.001, 1, 0.3)
-if args.task == 'GraphMultiGprompt':
+    pt = NodePrePrompt(args.dataset_name, args.hid_dim, nonlinearity, 0.9, 0.9, 0.1, 0.001, 1, 0.3, device=args.device)
+if args.pretrain_task == 'GraphMultiGprompt':
     nonlinearity = 'prelu'
-    pt = GraphPrePrompt(graph_list, input_dim, out_dim, args.dataset_name, args.hid_dim, nonlinearity,0.9,0.9,0.1,1,0.3)
-if args.task == 'GraphMAE':
+    pt = GraphPrePrompt(graph_list, input_dim, out_dim, args.dataset_name, args.hid_dim, nonlinearity,0.9,0.9,0.1,1,0.3, device=args.device)
+if args.pretrain_task == 'GraphMAE':
     pt = GraphMAE(dataset_name = args.dataset_name, gnn_type = args.gnn_type, hid_dim = args.hid_dim, gln = args.num_layer, num_epoch=args.epochs, device=args.device,
                   mask_rate=0.75, drop_edge_rate=0.0, replace_rate=0.1, loss_fn='sce', alpha_l=2)
 pt.pretrain()
